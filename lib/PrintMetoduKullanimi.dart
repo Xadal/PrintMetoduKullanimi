058cdb78e66f2db70c5d9a41c95a4e5448781675
Abdullah <Abdullahadal8.aa@gmail.com>
2022-01-24 01:47:01 +0300

    Print metodu kullanimi

---
+++ lib/PrintMetoduKullanimi.dart
@@ -0,0 +1,19 @@
+void main() {
+
+  print("MERHABA");
+
+  var ad  = "Abdullah";
+  int yas = 22;
+
+  print(yas);
+
+  //Metinsel ifadeye değişken ekleme;
+
+  print("$ad Malatyada $yas yıldır yaşamaktadır.");
+
+  int a = 25;
+  var b = 44;
+
+  //String ifadeye Birden değişken ekleme;
+  print("$a ve $b ün toplamı : ${a+b}");
+}
